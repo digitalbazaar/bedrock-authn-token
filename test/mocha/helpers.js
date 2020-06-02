4bce7c34b523629cc6e1d8a669b1eb123b79ab40
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-02 14:39:24 -0400

    Use deleteMany over remove + mongo 3 branches.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -18,7 +18,7 @@ exports.removeCollections = async (
   ]) => {
   await promisify(database.openCollections)(collectionNames);
   for(const collectionName of collectionNames) {
-    await database.collections[collectionName].remove({});
+    await database.collections[collectionName].deleteMany({});
   }
 };
 
