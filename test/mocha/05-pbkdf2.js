ca582904e2ba8427b6fc861853a051fe255eea47
Dave Longley <dlongley@digitalbazaar.com>
2022-05-21 17:28:39 -0400

    Add tests for reusing salt.

--- test/mocha/05-pbkdf2.js
+++ test/mocha/05-pbkdf2.js
@@ -24,6 +24,44 @@ describe('pbkdf2 API', () => {
     should.exist(phc);
     phc.should.be.an('object');
   });
+  it('should create the same pbkdf2 hash with the same salt', async () => {
+    const {
+      algorithm, salt, derivedBits, hash, phc
+    } = await brAuthnToken._pbkdf2.pbkdf2({
+      iterations: 1000,
+      secret: 'password',
+      saltSize: 16
+    });
+    should.exist(algorithm);
+    algorithm.should.be.an('object');
+    should.exist(salt);
+    (salt instanceof Uint8Array).should.equal(true);
+    should.exist(derivedBits);
+    (derivedBits instanceof Uint8Array).should.equal(true);
+    should.exist(hash);
+    hash.should.be.a('string');
+    hash.should.include('$pbkdf2-sha512$i=1000$');
+    should.exist(phc);
+    phc.should.be.an('object');
+    const {salt: saltB64} = phc;
+    saltB64.should.be.a('string');
+
+    const {hash: hash2} = await brAuthnToken._pbkdf2.pbkdf2({
+      iterations: 1000,
+      secret: 'password',
+      // pass salt as uint8array
+      salt
+    });
+    hash2.should.eql(hash);
+
+    const {hash: hash3} = await brAuthnToken._pbkdf2.pbkdf2({
+      iterations: 1000,
+      secret: 'password',
+      // pass salt as a string
+      salt: saltB64
+    });
+    hash3.should.eql(hash);
+  });
   it('should roundtrip a pch object', async () => {
     const {phc, hash} = await brAuthnToken._pbkdf2.pbkdf2({
       iterations: 1000,
