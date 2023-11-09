033e56f5ae2982b7095ab9f43d1faac3795eea1e
Dave Longley <dlongley@digitalbazaar.com>
2023-11-09 14:09:27 -0500

    Cause configured tester account challenges to be a static value.

--- test/test.config.js
+++ test/test.config.js
@@ -1,14 +1,21 @@
 /*!
- * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2023 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
 import {fileURLToPath} from 'node:url';
 import path from 'node:path';
 import '@bedrock/mongodb';
+import '@bedrock/authn-token';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
+
+// set tester email
+config['authn-token'].nonce.testerAccounts = [{
+  id: 'urn:uuid:a1fa7222-a019-440a-8f27-302d448f1c4d',
+  email: 'tester@example.com'
+}];
 
 // MongoDB
 config.mongodb.name = 'bedrock_authn_token_test';
