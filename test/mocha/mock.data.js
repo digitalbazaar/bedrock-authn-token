d8fc18011b953290013fc03c7038dc59a7a5d4be
Dave Longley <dlongley@digitalbazaar.com>
2023-11-09 14:09:27 -0500

    Cause configured tester account challenges to be a static value.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -1,12 +1,15 @@
 /*!
- * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2018-2023 Digital Bazaar, Inc. All rights reserved.
  */
 import {v4 as uuid} from 'uuid';
 
 export const mockData = {};
 const accounts = mockData.accounts = {};
 
-const emails = ['alpha@example.com', 'beta@example.com', 'gamma@example.com'];
+const emails = [
+  'alpha@example.com', 'beta@example.com', 'gamma@example.com',
+  'tester@example.com'
+];
 for(const email of emails) {
   accounts[email] = {};
   accounts[email].account = _createAccount(email);
@@ -15,7 +18,9 @@ for(const email of emails) {
 
 function _createAccount(email) {
   const newAccount = {
-    id: 'urn:uuid:' + uuid(),
+    // handle special `tester@example.com` case with static UUID
+    id: email === 'tester@example.com' ?
+      'urn:uuid:a1fa7222-a019-440a-8f27-302d448f1c4d' : 'urn:uuid:' + uuid(),
     email
   };
   return newAccount;
